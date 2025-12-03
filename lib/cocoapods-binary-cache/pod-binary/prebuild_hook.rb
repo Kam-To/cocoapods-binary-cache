3315811751c187557af9dc4cd2dcacd257367672
Kamto <kamtoto.au@gmail.com>
2025-12-03 11:36:41 +0800

    delete post install hook

--- lib/cocoapods-binary-cache/pod-binary/prebuild_hook.rb
+++ lib/cocoapods-binary-cache/pod-binary/prebuild_hook.rb
@@ -4,7 +4,3 @@ require_relative "helper/prebuild_sandbox"
 Pod::HooksManager.register("cocoapods-binary-cache", :pre_install) do |installer_context|
   PodPrebuild::PreInstallHook.new(installer_context).run
 end
-
-Pod::HooksManager.register("cocoapods-binary-cache", :post_install) do |installer_context|
-  PodPrebuild::PostInstallHook.new(installer_context).run
-end
